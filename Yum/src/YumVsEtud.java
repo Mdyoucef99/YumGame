8900b4be28704febfb4fbb3eab68207d2ad162ab
Youcef <mekki.d.youcef@outlook.fr>
2021-09-14 17:27:32 -0400

    Ajout du fichier initial du TP yum

---
+++ Yum/src/YumVsEtud.java
@@ -0,0 +1,76 @@
+package src;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+
+
+/*
+
+ * Programme principal qui démarre le jeu de YUM pour un seul joueur.
+ * 
+ * Une série de 5 dés est généré aléatoirement et le joueur a droit
+ * a changer les dés qu'il désire à deux reprises à moins qu'il les
+ * garde tous.
+ * 
+ * Par la suite, le programme offre toutes les possibilités de points 
+ * pouvant être joués et le joueur décide quel est son choix parmi 
+ * ces possibilités.
+ * 
+ * Dans le cadre du cours inf111 (voir énnoncé fourni).
+ * 
+ * Auteur : Mettez le nom de chaque membre du groupe qui a suffisamment 
+ *          contribué en écriture de code et de commentaires.
+ *          
+ * Auteur :
+ * Auteur :
+ * Auteur :
+ * Auteur :
+ * Auteur :
+ * 
+ * 
+ * Auteur : Pierre Bélisle
+ *          
+ * Version : Copyright A2021
+ */
+
+public class YumVsEtud {
+
+	
+	// Les constantes sont définies dans le module Constantes.java
+	// Si vous en ajoutez, faîtes-le ici.
+	
+	
+	// Permet la saisie de donnée au clavier en mode console.
+	public static Scanner clavier = new Scanner(System.in);
+
+	public static void main(String[] args){
+		
+		 int[] anArray = new int[5];
+		 
+		    RandomNumberInArray(anArray,1,6);
+		   ModAffichage.afficherDes(anArray);
+		 
+	    /* Traduisez ici l'algorithme du programme principal
+	     * décrit dans l'énoncé et commenter votre code au fur et à mesure.
+	     */
+	
+	    System.out.print("Merci d'avoir joue au YUM avec nous");
+	}
+	
+	/*
+	 * Écrivez TOUS vos sous-programmes ici.  Il y en a entre 15 et 20.
+	 */
+	public static int[] RandomNumberInArray(int[] array, int min, int max) {
+	    Random random = new Random();
+	    for (int i = 0; i < array.length; i++) 
+	    {
+	        array[i] = random.nextInt(max - min) + min;
+	    }
+	    return array;
+	}
+ 
+}
