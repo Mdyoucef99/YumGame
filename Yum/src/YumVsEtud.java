cc9f639a54f5f63cd7fae22f89d2b88d4b947e27
Youcef <mekki.d.youcef@outlook.fr>
2021-09-19 13:58:34 -0400

    Changed implementation of ReshuffleDice method

--- Yum/src/YumVsEtud.java
+++ Yum/src/YumVsEtud.java
@@ -47,25 +47,28 @@ public class YumVsEtud {
 
 	public static void main(String[] args){
 		
-		
 		   int[] ArrayDice = new int[5]; //creation des 5 des.
-		   
 		   InitialShuflle(ArrayDice); //génération des chiffre aléatoirement entre 1 et 6; 
+		   
 		   ModAffichage.afficherDes(ArrayDice);//affichage des dés 
 		   ModAffichage.afficherGrillePossibilite(ArrayDice);//Affichage grille possibilite pares shuffle initial 
-		   
+		  
 		   System.out.print("Entrer  les des  a changer - " );
 		   String NumberChangeOfDice = clavier.nextLine();//Input le string qui represent les des a changer 
-		   System.out.print(System.lineSeparator());//next line 
-		   ReshuffleDice(ArrayDice,NumberChangeOfDice); //changement des des 
 		   
+	       ReshuffleDice(ArrayDice,NumberChangeOfDice); //changement des des
+		
 		   
+		
+ 
+	     
+			   
 		   
+		 
 	    /* Traduisez ici l'algorithme du programme principal
 	     * décrit dans l'énoncé et commenter votre code au fur et à mesure.
 	     */
 		   
-	
 	    System.out.print("Merci d'avoir joue au YUM avec nous");
 	}
 	
@@ -75,6 +78,7 @@ public class YumVsEtud {
 	 */
 	public static int[] InitialShuflle(int[] array) //fonction qui permet de donner 5 des different
 	{   
+		IncrementTour++;
 	    Random random = new Random();
 	    for (int i = 0; i < array.length; i++) 
 	    {
@@ -85,26 +89,28 @@ public class YumVsEtud {
 	}
 				
 	public static void ReshuffleDice(int[] arrayParam, String Index) // fonction qui shuffle les des selectionner.
-	{	 IncrementTour++;
+	{	 
+		
+		 IncrementTour++;
 		 Random random = new Random();
-		 System.out.print("Nouvelle main de des" );
-		 System.out.print(System.lineSeparator());
-		 
 		 for(char c : Index.toCharArray()) 
 		 {
 			      int a=Integer.parseInt(String.valueOf(c));//Convert char to int 
 			      if(a <=0) 
 			      {
 			    	  System.out.println("player decided to not shuffle");
+			    	  ModAffichage.afficherDes(arrayParam);//affichage des dés 
+			    	  
 			      }
 			      
 			      else {
+			    	  
 			    	  arrayParam[a-1] = random.nextInt(Constantes.DES_MAX - Constantes.DES_MIN) + Constantes.DES_MIN; //reshuffle les des a la position demander 
-			      }
-				  
+			    	  System.out.println("Nouvelle main pour les Dice ");
+			    	  ModAffichage.afficherDes(arrayParam);//affichage des dés 
+			      }		  
 		 }
-		 ModAffichage.afficherGrillePossibilite(arrayParam);//affichage the possibilité dans la grille
-		 
+		
 	} 
 	
 }
