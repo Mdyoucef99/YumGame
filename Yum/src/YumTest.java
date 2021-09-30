fe032bd8640ce9e9ac08854c4e400c0e38367a1f
Youcef <mekki.d.youcef@outlook.fr>
2021-09-29 21:56:23 -0400

    Ajout test unitaire pour le relancer du des

--- Yum/src/YumTest.java
+++ Yum/src/YumTest.java
@@ -9,15 +9,41 @@ class YumTest {
 	@Test // Test unit pour initilashuffle procedure @Youcef mekki daouadji
 	void test_InitialShuffle() {
 
+		
+		System.out.print("\n Main de des : \n");
 		int[] array = new int[5];
 		YumVsEtud.InitialShuflle(array);
 		for(int i=0;i<array.length;i++) 
 		{
-		System.out.println(array[i]);
+		
+		System.out.print(array[i]);
 			
 		}
+		
 
 	}
+	
+	@Test
+	void test_Reshuffle() {  //Test unitaire  pour la   procedure de relancer les des  @Youcef mekki daouadji
+
+		System.out.print("Nouvelle main de des : \n ");
+		int[] array = {1,2,3,4,5};
+		YumVsEtud.ReshuffleDice(array,"235");
+		
+		for(int i=0;i<array.length;i++) 
+		{
+		
+		System.out.print(array[i]);
+			
+		}
+		
+	}
+	
+
+	
+	
+	
+	
 	
 
 }
